e4c3b7d91769c76d359fa93c3650be8d22efb65b
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-09-07 17:27:16 +0300

    add default roles

---
+++ Hotello.API/Migrations/20240907142633_AddDefaultRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Hotello.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "8f0fd3d5-ed9b-4fc5-9fcd-6220f46bef66", null, "Admin", "ADMIN" },
+                    { "b8baaf49-611a-42d1-ae40-b67a77b45766", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8f0fd3d5-ed9b-4fc5-9fcd-6220f46bef66");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b8baaf49-611a-42d1-ae40-b67a77b45766");
+        }
+    }
+}
